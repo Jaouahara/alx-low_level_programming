75b58963bd33461eb91542e8287370d390c60208
Jaouahara <mach.jaouahara@gmail.com>
2023-04-19 16:34:26 -0700

    FUNCTION

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,13 +16,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int e = 0;
 
-	while (i < 5)
+	while (e < 5)
 	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
-			return (ops[i].f);
-		i++;
+		if (s && s[0] == ops[e].op[0] && !s[1])
+			return (ops[e].f);
+		e++;
 	}
 
 	return (NULL);

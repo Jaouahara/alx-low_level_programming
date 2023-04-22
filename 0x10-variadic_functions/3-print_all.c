34202ecb0efcd90918918fe2dd8fbed196bccfed
Jaouahara <mach.jaouahara@gmail.com>
2023-04-21 19:38:50 -0700

    UPdateA

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,7 +7,7 @@
  */
 void format_char(char *separator, va_list af)
 {
-        printf("%s%c", separator, va_arg(af, int));
+	printf("%s%c", separator, va_arg(af, int));
 }
 
 /**
@@ -17,31 +17,32 @@ void format_char(char *separator, va_list af)
  */
 void format_int(char *separator, va_list af)
 {
-        printf("%s%d", separator, va_arg(af, int));
+	printf("%s%d", separator, va_arg(af, int));
 }
 
 /**
  * format_float -formats float
  * @separator: the string seprator
- * àaf: argument pointer
+ * @af: argument pointer
  */
 void format_float(char *separator, va_list af)
 {
-        printf("%s%f", separator, va_arg(af, double));
+	printf("%s%f", separator, va_arg(af, double));
 }
 
-/**format_string - formats string
+/**
+ * format_string - formats string
  * @separator: the string separator
  * @af: argument pointer
  */
 void format_string(char *separator, va_list af)
 {
-        char *string = va_arg(af, char *);
+	char *string = va_arg(af, char *);
 
-        switch ((int)(!string))
-                case 1:
-                        string = "(nil)";
-        printf("%s%s", separator, string);
+	switch ((int)(!string))
+		case 1:
+			string = "(nil)";
+	printf("%s%s", separator, string);
 }
 
 /**
@@ -50,32 +51,32 @@ void format_string(char *separator, va_list af)
  */
 void print_all(const char * const format, ...)
 {
-      | int a = 0, k;
-        char *separator = "";
-        va_list af;
-        token_t tokens[] = {
-                {"c", format_char},
-                {"i", format_int},
-                {"f", format_float},
-                {"s", format_string},
-                {NULL, NULL}
-        };
+	int a = 0, k;
+	char *separator = "";
+	va_list af;
+	token_t tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
 
-        va_start(ap, format);
-        while (format && format[a])
-        {
+	va_start(ap, format);
+	while (format && format[a])
+	{
 		k = 0;
-                while (tokens[k].token)
-                {
-                        if (format[a] ==tokens[k].tokens[0])
-                        {
-                                tokens[k].f(separator, af);
-                                separator = ", ";
-                        }
-                        k++;
-                }
-                a++;
-        }
-        printf("\n");
-        va_end(af);
+		while (tokens[k].token)
+		{
+			if (format[a] == tokens[k].tokens[0])
+			{
+				tokens[k].f(separator, af);
+				separator = ", ";
+			}
+			k++;
+		}
+		a++;
+	}
+	printf("\n");
+	va_end(af);
 }
